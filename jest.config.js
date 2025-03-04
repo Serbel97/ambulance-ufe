3a0a056d8ed5548ea25a7767f4987316a40e6ff6
Erik Belak <erik.belak@gmail.com>
2025-03-04 21:24:54 +0100

    Added ambulance waiting list API

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+export default {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "<rootDir>/node_modules/@stencil/core/testing/jest-preprocessor.js",
+    "^.+\\.(js|jsx)$": "babel-jest"
+  },
+  transformIgnorePatterns: ["/node_modules/"],
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(tsx?|jsx?)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"]
+};
+
+// extension .cjs
+// module.exports = {
+//   "roots": [
+//     "<rootDir>/src"
+//   ],
+//   "transform": {
+//     "^.+\\.(ts|tsx)$": "<rootDir>/node_modules/@stencil/core/testing/jest-preprocessor.js",
+//     "^.+\\.(js|jsx)$": "babel-jest",
+//   },
+//   transformIgnorePatterns: ["/node_modules/"],
+//   "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(tsx?|jsx?)$",
+//   "moduleFileExtensions": [
+//     "ts",
+//     "tsx",
+//     "js",
+//     "json",
+//     "jsx"
+//   ]
+// }
